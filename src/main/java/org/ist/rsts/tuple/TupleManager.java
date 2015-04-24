e346519de20edd1871ec3b4da22327c867af6185
ashansa <ashansa.perera@gmail.com>
2015-04-24 12:36:05 +0100

    add blocking if read doesn't find a match

---
+++ src/main/java/org/ist/rsts/tuple/TupleManager.java
@@ -0,0 +1,68 @@
+package org.ist.rsts.tuple;
+
+import org.ist.rsts.ServerGroup;
+
+import java.io.IOException;
+import java.util.Vector;
+
+public class TupleManager {
+
+    private TupleSpace tupleSpace= new TupleSpace();
+    //TODO: can we have > 1 pending req? since we block when there is a pending req
+    Vector<Tuple> pendingReadRequests = new Vector<Tuple>();
+    Vector<Tuple> pendingTakeRequests = new Vector<Tuple>();
+    ServerGroup server;
+
+    public TupleManager(ServerGroup server) {
+        this.server = server;
+    }
+
+    public void writeTuple(Tuple tuple) {
+        tupleSpace.write(tuple);
+        if(pendingReadRequests.size() > 0)
+            servePendingReadRequests(tuple);
+    }
+
+    public Tuple readTuple(Tuple template) {
+        Vector<Tuple> results = tupleSpace.read(template);
+        if(results.size() > 0) {
+            return results.firstElement();
+        }
+        else {
+            pendingReadRequests.add(template);
+            return null;
+        }
+    }
+
+    public void takeTuple(Tuple template) {
+        tupleSpace.take(template);
+    }
+
+
+    private void servePendingReadRequests(Tuple newTuple) {
+        for (int i = 0; i < pendingReadRequests.size(); i++) {
+            boolean match = isAMatch(pendingReadRequests.get(i), newTuple);
+            if(match) {
+                System.out.println("found a match for a pending.....");
+                try {
+                    server.sendResultsNotificationToClient(newTuple, Type.READ);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private boolean isAMatch(Tuple template, Tuple tuple) {
+        String[] tupleValues = tuple.getValues();
+        String[] templateElements = template.getValues();
+        //checking 1st value
+        if (("*".equals(templateElements[0]) || tupleValues[0].equals(templateElements[0])) &&
+                ("*".equals(templateElements[1]) || tupleValues[1].equals(templateElements[1])) &&
+                ("*".equals(templateElements[2]) || tupleValues[2].equals(templateElements[2]))) {
+            return true;
+        }
+
+        return false;
+    }
+}
